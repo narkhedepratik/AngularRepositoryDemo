7fba096b32bd52c27014e9c2a3bd01a44c3fb534
pratik narkhede <pratiknarkhede039@gmail.com>
2022-11-15 15:44:01 +0530

    modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserDetailsComponent } from './modules/user-details/user-details.component';
-import { ViewUserComponent } from './modules/view-user/view-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserDetailsComponent,
-    ViewUserComponent
+   
   ],
   imports: [
     BrowserModule,
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ViewUserComponent } from './view-user/view-user.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ViewUserComponent,
+    UserDetailsComponent
+  ],
   imports: [
     CommonModule
   ]
